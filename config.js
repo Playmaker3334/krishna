568ba7f85bf76b204d504e46a7dabf12eb321ae1
Call Me Maker <133881086+Playmaker3334@users.noreply.github.com>
2024-07-11 23:34:34 -0600

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,47 @@
+const _0x1367e0 = _0x28f5;
+
+function _0x28f5(_0x593f0a, _0x396d4c) {
+    const _0x9741bd = _0x9741();
+    return _0x28f5 = function(_0x28f56f, _0x4143fa) {
+        _0x28f56f = _0x28f56f - 0x88;
+        let _0x4efccb = _0x9741bd[_0x28f56f];
+        return _0x4efccb;
+    }, _0x28f5(_0x593f0a, _0x396d4c);
+}
+
+(function(_0x354c89, _0x167a5e) {
+    const _0x441244 = _0x28f5,
+          _0x24b325 = _0x354c89();
+
+    while (!![]) {
+        try {
+            const _0x55e541 = -parseInt(_0x441244(0x91)) / 0x1 + parseInt(_0x441244(0x88)) / 0x2 * (parseInt(_0x441244(0x8e)) / 0x3) + parseInt(_0x441244(0x8f)) / 0x4 + parseInt(_0x441244(0x92)) / 0x5 * (-parseInt(_0x441244(0x89)) / 0x6) + parseInt(_0x441244(0x8b)) / 0x7 * (parseInt(_0x441244(0x93)) / 0x8) + parseInt(_0x441244(0x94)) / 0x9 + -parseInt(_0x441244(0x8c)) / 0xa;
+            if (_0x55e541 === _0x167a5e)
+                break;
+            else
+                _0x24b325.push(_0x24b325.shift());
+        } catch (_0x5c53b3) {
+            _0x24b325.push(_0x24b325.shift());
+        }
+    }
+}(_0x9741, 0xa4a68));
+
 const emailjsConfig = {
-    serviceId: 'service_6uxpien',
-    templateId: 'template_r5m9ojf',
-    userId: '25jcYMEfjNlabt7Av'
+    'serviceId': _0x1367e0(0x90),
+    'templateId': _0x1367e0(0x8d),
+    'userId': '25jcYMEfjNlabt7Av'
 };
-console.log("config.js loaded");
+
+console['log'](_0x1367e0(0x8a));
+
+function _0x9741() {
+    const _0x234eac = [
+        '13337250SdjsDF', 'template_r5m9ojf', '21eNdrMd', '4395128DOpaRL', 'service_6uxpien',
+        '631498ItjxtD', '23085FXNCdi', '2208xpfSWu', '3763008CYNHRc', '40876shIiSf', 
+        '78Mayall', 'config.js loaded', '26369UjppgY'
+    ];
+    _0x9741 = function() {
+        return _0x234eac;
+    };
+    return _0x9741();
+}
